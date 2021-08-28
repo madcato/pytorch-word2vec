8a9458fb2036365fd5c50c24dc32db4620f24a94
Daniel Vela <veladan@me.com>
2021-08-28 17:34:57 +0000

    muli-gpu

--- word2vec/train.py
+++ word2vec/train.py
@@ -7,7 +7,6 @@ print('Using {} device'.format(device))
 
 def train_epoch(dataset, model, learning_rate, batch_size):
     dataloader = DataLoader(dataset, batch_size=batch_size)
-    model = model.to(device)  # it's hudge important to move model to device, before creating optimizer
     loss_fn = nn.NLLLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0, weight_decay=0, nesterov=False)
     size = len(dataloader.dataset)
@@ -28,6 +27,12 @@ def train_epoch(dataset, model, learning_rate, batch_size):
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 def train(dataset, model, learning_rate = 1, batch_size = 64, epochs = 15):
+    if torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        model = nn.DataParallel(model)
+    
+    model = model.to(device)  # it's hudge important to move model to device, before creating optimizer
+    
     for t in range(epochs):
         print("")
         print(f"Epoch {t+1}\n-------------------------------")
